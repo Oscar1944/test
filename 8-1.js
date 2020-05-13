da444f9277d4f2995928b9bd4fdb8446464afae6
Oscar <h194420kk2@gmail.com>
2020-05-13 21:31:40 +0800

    8-1.js modified version

---
+++ 8-1.js
@@ -0,0 +1,13 @@
+let arr =['RADAR','WARTER START','MILK KLIM','RESERVERED','IWI',"ABBA"];
+function palindrome(arry,callback){
+    for(let n of arry){
+        n =Array.from(n);
+        let n1 =n.reduce((acc,elem)=>acc+elem);
+        let n2 =n.reduceRight((acc,elem)=>acc+elem);
+        if(n1===n2){
+            callback(n1);
+        }
+    }
+}
+palindrome(arr,console.log);
+console.log("this file is for git test")
